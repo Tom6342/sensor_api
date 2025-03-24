ab013ef9a405ae1c39a1f148e88980afa496c622
Tom <tom6342@gmail.com>
2025-03-24 14:55:34 +0100

    use sys.argv to retrieve command line arguments

--- fake_data_app/sensor.py
+++ fake_data_app/sensor.py
@@ -1,5 +1,6 @@
 from datetime import date
 import numpy as np
+import sys
 
 class VisitSensor:
         """
@@ -43,5 +44,11 @@ class VisitSensor:
             return np.floor(visit)
 
 if __name__ == "__main__":
+    if len(sys.argv)>1:
+        year, month, day, = [int(v) for v in sys.argv[1].split("-")]
+    else:
+        year, month, day = 2023, 10, 25
+    queried_date = date(year, month, day)
+
     capteur = VisitSensor( 1500,  150)
-    print(capteur.simulate_visit(date(year=2023, month=10, day=25)))
+    print(capteur.simulate_visit(queried_date))
